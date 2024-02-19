87e81ce870cfc7292200f0487294c1eb4a4d6d83
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2024-02-19 14:40:55 +0900

    一旦完成した実装

--- index.js
+++ index.js
@@ -1,47 +1,119 @@
 import puppeteer from 'puppeteer'
 import Papa from 'papaparse'
-
-const iterateLen = 6
-const scrapeLen = 2500
-
-const kwTbl = [
-    ['能登', '石川'],
-    ['地震', '震災'],
-]
-
-const cnt = { value: 0 }
-
-const initCsvStr = `
-url,ttl,desc
-https://www.kantei.go.jp/,,
-https://www.pref.ishikawa.lg.jp/,,
-https://www.gov-online.go.jp/,,
-`.slice(1, -1)
-
-const { data } = Papa.parse(initCsvStr, {
-    header: true,
-})
-
-const li = Object.fromEntries(
-    data.map(({ url, ttl, desc }) => [
-        url,
-        {
-            url,
-            ttl,
-            desc,
-        },
-    ])
-)
+import fs from 'fs'
 
 const handler = async _ => {
+    const nowDate = new Date()
+    const nowDateStr = nowDate.toISOString()
+
+    const timeoutMs = 45000
+
+    const iterateLen = 3
+    const scrapeLen = 250
+
+    const placeNameArr = [
+        '志賀',
+        '七尾',
+        '輪島',
+        '珠洲',
+        '穴水',
+        '中能登',
+        '能登町',
+        '金沢市',
+        '小松',
+        '加賀市',
+        '羽咋市',
+        'かほく',
+        '能美市',
+        '宝達志水',
+        '長岡',
+        '中央区',
+        '南区',
+        '西区',
+        '西蒲区',
+        '三条',
+        '柏崎',
+        '見附',
+        '燕市',
+        '糸魚川',
+        '妙高',
+        '上越市',
+        '佐渡',
+        '南魚沼',
+        '阿賀',
+        '刈羽',
+        '富山市',
+        '高岡',
+        '氷見',
+        '小矢部',
+        '南砺',
+        '射水',
+        '舟橋',
+        'あわら',
+    ]
+
+    const kwTbl = [
+        ['能登', '北陸', '石川', '新潟', '富山', '福井'].concat(placeNameArr),
+        ['地震', '震災', '災害', '防災', '避難'],
+    ]
+
+    const cnt = { value: 0 }
+
+    const initCsv = {
+        value: '',
+        default: `
+url,ttl,desc,createdAt,updatedAt
+https://www.pref.ishikawa.lg.jp/shimachi.html,石川県内市町のページ | 石川県,,,
+https://caidr.jp/2024/01/02/r6_noto/,令和6年能登半島地震における情報リンク集 – AI防災協議会,,,
+https://www.mlit.go.jp/river/bousai/olympic/index.html,国土交通省　防災ポータル,,,
+https://www.jishin.go.jp/link/,関連機関リンク | 地震本部,,,
+`.slice(1, -1),
+    }
+
+    const nowFileName = nowDate
+        .toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })
+        .replace(/\s/, 'T')
+        .replaceAll(/[/:]/g, '')
+
+    try {
+        initCsv.value = fs.readFileSync('./sheet.csv', { encoding: 'utf8' })
+    } catch (err) {
+        console.log('err: CSV file cannot open')
+        // console.log(err)
+    }
+
+    fs.writeFileSync(`${nowFileName}.csv`, initCsv.value || initCsv.default, {
+        encoding: 'utf8',
+    })
+
+    const { data } = Papa.parse(initCsv.value || initCsv.default, {
+        header: true,
+    })
+
+    const li = Object.fromEntries(
+        data.map(({ url, place, ttl, desc, createdAt, updatedAt }) => [
+            url,
+            {
+                url,
+                place,
+                ttl,
+                desc,
+                createdAt,
+                updatedAt,
+            },
+        ])
+    )
+
     const browser = await puppeteer.launch()
 
     const page = await browser.newPage()
 
-    const getPage = async url => {
-        new Promise(r => setTimeout(r, 15))
+    page.setDefaultNavigationTimeout(timeoutMs)
 
-        // console.info('fetch: ', url)
+    const getPage = async url => {
+        new Promise(r => setTimeout(r, Math.random() * 15))
+
+        console.info('fetch: ', url)
 
         try {
             if (cnt.value > scrapeLen) {
@@ -58,42 +130,78 @@ const handler = async _ => {
 
             const pageTitleStr = typeof titleVal === 'string' ? titleVal : ''
 
-            const articleElm = await page.$('article')
-            const mainElm = await page.$('main')
-            const bodyElm = await page.$('body')
+            const mainQueryStr =
+                'article, main, .contents [class*="main" i], [class*="content" i], [class*="container" i], [class*="container" i], [class*="body" i], body'
 
-            const elm = articleElm || mainElm || bodyElm
+            const mainElm = await page.$(mainQueryStr)
 
-            const txtRes = await elm.getProperty('innerText')
+            const txtRes = await mainElm.getProperty('innerText')
             const txtVal = await txtRes.jsonValue()
+            const txtVal1Line = txtVal
+                .replaceAll(/[\s\t\u3000]+/g, ' ')
+                .replaceAll('\n', '')
+                ?.trim()
 
-            const matchKw = kwTbl.every(kwArr =>
-                kwArr.some(kw => txtVal.includes(kw))
+            const isMatchKw = kwTbl.every(kwArr =>
+                kwArr.some(kw => txtVal1Line.includes(kw))
             )
 
-            if (matchKw) {
-                const urlObj = new URL(url)
-                const normalUrl = `${urlObj.origin}${urlObj.pathname}`
+            const urlObj = new URL(url)
+            const normalUrl = `${urlObj.origin}${urlObj.pathname}`
+
+            if (isMatchKw) {
+                const ttl =
+                    pageTitleStr.replaceAll('\n', '')?.trim() || '無題のページ'
 
                 const linkElmLi = await page.$$('a')
 
-                li[url] = {
-                    url: normalUrl,
-                    ttl: pageTitleStr.replaceAll('\n', '')?.trim(),
-                    desc: txtVal.slice(0, 15).replaceAll('\n', '') || '',
-                }
+                placeNameArr.forEach(placeName => {
+                    if (!txtVal1Line.includes(placeName)) {
+                        return
+                    }
+
+                    const pos = txtVal1Line.indexOf(placeName) - 125
+
+                    const startPos = Math.max(pos, 0)
+
+                    const desc = txtVal1Line.slice(startPos, startPos + 150)
+
+                    const urlWithFrag = normalUrl + '#:~:text=' + placeName
+
+                    li[urlWithFrag] = {
+                        url: urlWithFrag,
+                        place: placeName,
+                        ttl,
+                        desc,
+                        createdAt: nowDateStr,
+                        updatedAt: nowDateStr,
+                    }
+
+                    const { createdAt } = li[normalUrl] || nowDateStr
+
+                    li[normalUrl] = {
+                        url: normalUrl,
+                        ttl,
+                        desc: '',
+                        createdAt,
+                        updatedAt: nowDateStr,
+                    }
+                })
 
                 for (const linkElm of linkElmLi) {
-                    // const txtRes = await linkElm.getProperty('innerText')
+                    const labelRes = await linkElm.getProperty('innerText')
                     const hrefRes = await linkElm.getProperty('href')
 
-                    // const txtVal = await txtRes.jsonValue()
+                    const labelVal = await labelRes.jsonValue()
+                    const labelValTrim = labelVal.replaceAll('\n', '').trim()
                     const hrefVal = await hrefRes.jsonValue()
 
                     const hrefObj = new URL(hrefVal)
                     const normalHref = `${hrefObj.origin}${hrefObj.pathname}`
 
-                    const matchHref = [
+                    const isNewcomer = li[normalHref] == null
+
+                    const isMatchDomain = [
                         '.go.jp',
                         '.lg.jp',
                         '.ishikawa.jp',
@@ -101,19 +209,63 @@ const handler = async _ => {
                         '.fukui.jp',
                         '.niigata.jp',
                         '.hodatsushimizu.jp',
-                    ].some(
-                        domain =>
-                            hrefObj.origin.endsWith(domain) &&
-                            !hrefObj.pathname.endsWith('.pdf')
-                    )
+                        '.caidr.jp',
+                        // '.ac.jp',
+                    ].some(domain => hrefObj.origin.endsWith(domain))
 
-                    if (li[normalHref] == null && matchHref) {
-                        li[normalHref] = {
-                            url: normalHref,
-                            ttl: '',
-                            desc: '',
+                    const isMatchPdf = hrefObj.pathname.endsWith('.pdf')
+                    const isMatchBlackExt = [
+                        '.xml',
+                        '.zip',
+                        '.rdf',
+                        '.xlsx',
+                        'docx',
+                        '.xls',
+                        '.doc',
+                        '.jpg',
+                        '.jpeg',
+                        '.png',
+                    ].some(ext => hrefObj.pathname.endsWith(ext))
+
+                    if (isMatchDomain && isNewcomer) {
+                        const isMatchTtlKw = kwTbl.every(kwArr =>
+                            kwArr.some(kw => labelValTrim.includes(kw))
+                        )
+
+                        if (isMatchPdf && isMatchTtlKw) {
+                            const ttlLabel = labelValTrim
+
+                            if (isMatchTtlKw) {
+                                li[normalHref] = {
+                                    url: normalHref,
+                                    ttl: ttlLabel,
+                                    desc: `「${ttl}」(${hrefObj.hostname})からのPDFリンク`,
+                                    createdAt: nowDateStr,
+                                    updatedAt: nowDateStr,
+                                }
+                            }
+                        } else if (!isMatchPdf && !isMatchBlackExt) {
+                            const ttlLabel = labelValTrim || '不明なページ'
+
+                            li[normalHref] = {
+                                url: normalHref,
+                                ttl: ttlLabel,
+                                desc: `「${ttl}」(${hrefObj.hostname})からのページリンク`,
+                                createdAt: nowDateStr,
+                                updatedAt: '',
+                            }
                         }
                     }
+                }
+            } else {
+                const { createdAt } = li[normalUrl] || nowDateStr
+
+                li[normalUrl] = {
+                    url: normalUrl,
+                    ttl: '',
+                    desc: '',
+                    createdAt,
+                    updatedAt: nowDateStr,
                 }
             }
         } catch (err) {
@@ -130,29 +282,46 @@ const handler = async _ => {
 
     for (let i = 0; i < iterateLen; i++) {
         for (const item of Object.values(li)) {
-            if (item.ttl == '') {
+            if (item.updatedAt == '') {
                 await getPage(item.url)
             }
         }
     }
 
     await browser.close()
+
+    const idxItm = item => {
+        const { host, pathname } = new URL(item.url)
+
+        const prefix = [
+            item.place === '',
+            pathname.endsWith('.pdf'),
+            item.ttl === '',
+            item.desc === '',
+            item.updatedAt === '',
+        ]
+            .map(bool => (bool ? '-' : '!'))
+            .join('')
+
+        return (
+            prefix +
+            host.split('.').reverse().join('/') +
+            ' ' +
+            pathname +
+            ' ' +
+            item.place
+        )
+    }
+
+    const csvStr = Papa.unparse({
+        fields: ['url', 'place', 'ttl', 'desc', 'createdAt', 'updatedAt'],
+        data: Object.values(li).sort((a, b) =>
+            idxItm(a) > idxItm(b) ? 1 : -1
+        ),
+    })
+
+    fs.writeFileSync('sheet.csv', csvStr, { encoding: 'utf8' })
+    // console.log(Object.entries(li).length)
 }
 
-await handler()
-
-const idxItm = item => {
-    const { host, pathname } = new URL(item.url)
-
-    const prefix = item.desc === '' ? '-' : '! '
-
-    return prefix + host.split('.').reverse().join('/') + ' ' + pathname
-}
-
-const csvStr = Papa.unparse({
-    fields: ['url', 'ttl', 'desc'],
-    data: Object.values(li).sort((a, b) => (idxItm(a) > idxItm(b) ? 1 : -1)),
-})
-
-console.log(csvStr)
-// console.log(Object.entries(li).length)
+handler()
